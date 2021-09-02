b1e87e26fe3752a2f03a6ea26c304e318975d981
TahsinUchiha <alton255@yahoo.co.uk>
2021-09-03 00:01:15 +0100

    added pricing calculator js

---
+++ ResponsiveWeb/Projects/Landing-Page/app.js
@@ -0,0 +1,18 @@
+/* Price Calculator */ 
+const metalDictionary = {
+    "IRON": 1,
+    "COBALT": 40.38,
+    "NICKEL":3.99,
+    "RUTHENIUM": 435.43,
+    "RHODIUM": 9435.41,
+    "PALLADIUM": 2235.23,
+    "OSMIUM": 3435.33,
+    "IRIDIUM": 2354.13,
+    "PLATINUM": 4345.56,
+
+}
+
+const priceOfMetal = (metal, weight) => {
+    return Math.round((metalDictionary[metal] * weight)*1000)/1000;
+}
+
